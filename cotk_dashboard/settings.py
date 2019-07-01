32f430a9b8fbb47769f92e3a2c4a8d36ae0b5b5e
hzhwcmhf <hzhwcmhf@gmail.com>
2019-07-01 16:02:43 +0800

    update docker

--- cotk_dashboard/settings.py
+++ cotk_dashboard/settings.py
@@ -80,7 +80,7 @@ DATABASES = {
 		'NAME': 'mysql',
 		'USER': 'root',
 		'PASSWORD': 'itsasecret',
-		'HOST': '127.0.0.1',
+		'HOST': 'db',
 		'PORT': '3306'
 	}
 }
--- dashboard_mysql.sql
+++ dashboard_mysql.sql
@@ -1,3 +1,3 @@
-CREATE USER 'root'@'%' IDENTIFIED BY 'itsasecret';
+CREATE USER 'root'@'%' IDENTIFIED WITH mysql_native_password BY 'itsasecret';
 GRANT ALL PRIVILEGES ON *.* TO 'root'@'%';
 FLUSH PRIVILEGES;
