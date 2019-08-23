b6aa7d5c4173f7dd1d0b7a89941d22373e4a9add
hzhwcmhf <hzhwcmhf@gmail.com>
2019-08-23 13:20:29 +0800

    update static script url

--- cotk_dashboard/settings.py
+++ cotk_dashboard/settings.py
@@ -53,6 +53,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'dashboard.middleware.SettingsMiddleware',
 ]
 
 ROOT_URLCONF = 'cotk_dashboard.urls'
